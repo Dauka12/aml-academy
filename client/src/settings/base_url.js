25af1375ce14ed78a9e0d2239cb0ea78fcd2bef6
Dauka12 <dauka2707@gmail.com>
2024-08-22 13:50:41 +0500

    adaw

--- client/src/settings/base_url.js
+++ client/src/settings/base_url.js
@@ -1,4 +1,4 @@
-// const base_url = 'https://amlacademy.kz';
-const base_url = 'http://10.12.17.23:8443';
+const base_url = 'https://amlacademy.kz';
+// const base_url = 'http://10.12.17.23:8443';
 
 export default base_url;
