3bc5af62961fcdb581752618ee17f655ccedfe21
Dauka12 <dauka2707@gmail.com>
2025-03-17 12:14:25 +0500

    c

--- client/src/features/olympiad/store/index.ts
+++ client/src/features/olympiad/store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice.ts';
 import examReducer from './slices/examSlice.ts';
 import registrationReducer from './slices/registrationSlice.ts';
+import testSessionReducer from './slices/testSessionSlice.ts';
 
 export const olympiadStore = configureStore({
   reducer: {
     auth: authReducer,
     registration: registrationReducer,
-    exam: examReducer
+    exam: examReducer,
+    testSession: testSessionReducer
   },
   devTools: process.env.NODE_ENV !== 'production',
 });
