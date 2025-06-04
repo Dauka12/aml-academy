c0e1936b10fa85e688edf77614a4eae186b46dd6
Dauka12 <dauka2707@gmail.com>
2025-06-04 23:38:50 +0500

    asd

--- client/start-quiz-test.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Quiz Navigation Test Page...
-echo Opening in default browser...
-start "" "%~dp0quiz-navigation-test.html"
-echo.
-echo Test page opened! 
-echo Check the browser for testing instructions.
-echo.
-pause
