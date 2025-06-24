3afe7f931ecd05c357c927b0f54ddf8a6c3884bd
aibeek <aibekfestival23@gmail.com>
2025-06-24 11:51:36 +0500

    bags

--- client/src/pages/all-news/index.jsx
+++ client/src/pages/all-news/index.jsx
@@ -8,7 +8,7 @@ import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import calendarIcon from "../../assets/icons/calendar.svg";
 import Footer from "../../components/footer/Footer";
-import Header from "../../components/header/Header";
+import Header from "../../components/header/v2";
 import Button from "../../components/UI/button/Button";
 import { useStyle } from "../../components/VisualModal/StyleContext";
 import VisualModal from "../../components/VisualModal/VisualModal";
